47b0ad5cf946f56cc3aba171734da4108cd6f956
Galin Kalinov <71660913+GalinKalinovY@users.noreply.github.com>
2020-10-22 16:43:58 +0200

    Update Billiards.cpp
    Afegim Galin

--- Billiards/src/Billiards.cpp
+++ Billiards/src/Billiards.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "Hola crack fiera" << endl; // prints !!!Hello World!!!
+	cout << "Hola crack fiera soc Galom" << endl; // prints !!!Hello World!!!
 	return 0;//hola hola caracola
 }
